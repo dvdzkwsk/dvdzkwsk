1eb93b78414685738c1edc6b39ba877cb8ffd06c
David Zukowski <6439050+davezuko@users.noreply.github.com>
2024-02-10 10:34:33 -0800

    working on build system

--- packages/build/src/index.ts
+++ packages/build/src/index.ts
@@ -2,64 +2,213 @@ import * as fs from "fs"
 import * as path from "path"
 import * as esbuild from "esbuild"
 import {Logger} from "@dvdzkwsk/logger"
-import {buildWebApp} from "./WebApp.js"
-import {buildNodeApp} from "./NodeApp.js"
+import {EsbuildPluginPreact} from "./plugins/EsbuildPluginPreact.js"
 
 const logger = new Logger("Build")
 
+type EnvVars = Record<string, string>
+
+export interface BuildContext {
+	buildOptions: BuildOptions
+}
+export function context(buildOptions: BuildOptions): BuildContext {
+	buildOptions = structuredClone(buildOptions)
+	buildOptions.env["NODE_ENV"] ??= process.env.NODE_ENV ?? buildOptions.mode
+	buildOptions.env["NODE_ENV"] = "asdadsada"
+	return {
+		buildOptions,
+	}
+}
+
 export interface BuildOptions {
-	cwd: string
-	dev: boolean
+	target: "node" | "browser"
 	mode: "development" | "production"
-	esbuild: esbuild.BuildOptions
+	src: string
+	dst: string
+	servedir?: string
+	entrypoints: string[]
+	env: EnvVars
 }
 
-export async function build(options: BuildOptions) {
-	if (!options.dev) {
-		const dist = path.join(options.cwd, "dist")
-		if (fs.existsSync(dist)) {
-			logger.debug("build", "remove dist")
-			await fs.promises.rm(path.join(options.cwd, "dist"), {
-				recursive: true,
-				force: true,
-			})
-		}
+export async function buildToDisk(context: BuildContext) {
+	const {buildOptions} = context
+	if (fs.existsSync(buildOptions.dst)) {
+		logger.debug("rmdir", "destination exists; removing it.", {
+			path: buildOptions.dst,
+		})
+		await fs.promises.rm(buildOptions.dst, {
+			recursive: true,
+			force: true,
+		})
 	}
-
-	loadEnvVars(options.cwd)
-
-	const pkg = JSON.parse(
-		await fs.promises.readFile(
-			path.join(options.cwd, "package.json"),
-			"utf8",
-		),
+	const esbuildBuildOptions = buildEsbuildOptions(buildOptions)
+	const result = await esbuild.build(esbuildBuildOptions)
+	logger.info("buildToDisk", "finished esbuild")
+	if (result.metafile) {
+		logger.debug(
+			"buildToDisk",
+			"analyze esbuild metafile\n" +
+				(await esbuild.analyzeMetafile(result.metafile)),
+		)
+	}
+	await fs.promises.cp(
+		path.resolve(buildOptions.src, "public"),
+		buildOptions.dst,
+		{
+			recursive: true,
+		},
 	)
-	switch (pkg.packageType) {
-		case "NodeApp":
-			return buildNodeApp(options)
-		case "WebApp":
-			return buildWebApp(options)
-		default:
-			throw logger.newError("build", "unknown package type", {
-				packageType: pkg.packageType,
-			})
+	// await updateHashedAssetPaths(buildOptions.src, result)
+}
+
+function buildEsbuildOptions(
+	buildOptions: BuildOptions,
+	options?: {
+		hashAssets?: boolean
+	},
+): esbuild.BuildOptions {
+	let esbuildBuildOptions: esbuild.BuildOptions = {}
+	switch (buildOptions.target) {
+		case "browser":
+			esbuildBuildOptions = {
+				absWorkingDir: buildOptions.src,
+				entryPoints: buildOptions.entrypoints,
+				outdir: path.resolve(buildOptions.dst, "assets"),
+				assetNames: "[name]",
+				entryNames: "[name]",
+				bundle: true,
+				format: "esm",
+				platform: "browser",
+				target: "esnext",
+				pure: [],
+				plugins: [EsbuildPluginPreact()],
+				sourcemap: "linked",
+				define: stringifyEnvVars(buildOptions.env),
+			}
+		case "node":
+	}
+	switch (buildOptions.mode) {
+		case "development":
+			esbuildBuildOptions.minify = false
+			// TODO(david): figure out a way to get esbuild to roll up very many tiny chunks into one
+			// before enabling this. It can be worse for perf if we have to make many network requests
+			// for extremely small amounts of code.
+			esbuildBuildOptions.splitting = false
+			esbuildBuildOptions.write = false
+			break
+		case "production":
+			esbuildBuildOptions.minify = true
+			// TODO(david): figure out a way to get esbuild to roll up very many tiny chunks into one
+			// before enabling this. It can be worse for perf if we have to make many network requests
+			// for extremely small amounts of code.
+			esbuildBuildOptions.splitting = false
+			esbuildBuildOptions.metafile = true
+			break
+	}
+	if (options?.hashAssets) {
+		esbuildBuildOptions.assetNames = "[name]-[hash]"
+		esbuildBuildOptions.entryNames = "[name]-[hash]"
+	}
+	return esbuildBuildOptions
+}
+
+interface ServeOptions {
+	port?: number
+}
+export async function serve(context: BuildContext, options?: ServeOptions) {
+	const {servedir} = context.buildOptions
+	if (!servedir) {
+		throw logger.newError("serve", "missing servedir in build options")
+	}
+	const esbuildBuildOptions = buildEsbuildOptions(context.buildOptions)
+	esbuildBuildOptions.outdir = path.resolve(servedir, "assets")
+	const esbuildContext = await esbuild.context(esbuildBuildOptions)
+	const esbuildServeOptions: esbuild.ServeOptions = {
+		port: options?.port ?? 3000,
+		servedir: servedir,
+		fallback: path.join(servedir, "index.html"),
+	}
+	console.log({
+		context,
+		esbuildBuildOptions,
+		env: context.buildOptions.env,
+		esbuildServeOptions,
+	})
+	const server = await esbuildContext.serve(esbuildServeOptions)
+	logger.info("serve", `server running at: http://localhost:${server.port}`)
+}
+
+async function updateHashedAssetPaths(
+	src: string,
+	result: esbuild.BuildResult,
+) {
+	const outputs = Object.entries(result.metafile!.outputs).map((o) => {
+		return {
+			outputFile: o[0],
+			...o[1],
+		}
+	})
+	const mainJS = outputs.find((o) => {
+		return (
+			o.entryPoint?.endsWith("main.ts") ||
+			o.entryPoint?.endsWith("main.tsx")
+		)
+	})!
+	const mainCSSOutputFile =
+		mainJS.cssBundle ||
+		outputs.find((o) => {
+			return o.entryPoint?.endsWith("main.css")
+		})?.outputFile!
+
+	const htmlFiles = ["dist/index.html"]
+
+	for (const htmlFile of htmlFiles) {
+		let html = fs.readFileSync(path.resolve(src, htmlFile), "utf8")
+		html = html.replace(
+			"/assets/main.js",
+			mainJS.outputFile.replace("dist/assets", "/assets"),
+		)
+		if (mainCSSOutputFile) {
+			html = html.replace(
+				"/assets/main.css",
+				mainCSSOutputFile.replace("dist/assets", "/assets"),
+			)
+		}
+		await fs.promises.writeFile(path.resolve(cwd, htmlFile), html, "utf8")
 	}
 }
 
-function loadEnvVars(cwd: string) {
-	const envFile = path.join(cwd, ".env")
-	if (!fs.existsSync(envFile)) {
-		logger.debug("loadEnvVars", ".env file does not exist", {path: envFile})
-		return
-	}
-	const txt = fs.readFileSync(envFile, "utf8")
+export async function loadEnvFile(
+	filepath: string,
+	options?: {
+		only?: string[]
+	},
+): Promise<Record<string, string>> {
+	const env: Record<string, string> = {}
+	const txt = await fs.promises.readFile(filepath, "utf8")
 	for (const line of txt.split("\n")) {
 		if (line.trim().startsWith("#")) {
 			continue
 		}
-		const [key, value] = line.trim().split("=")
+		const [key, value] = line
+			.trim()
+			.split("=")
+			.map((v) => v.trim())
+
+		if (options?.only && !options.only.includes(key)) {
+			continue
+		}
 		if (key && value) {
-			process.env[key] = value
+			env[key] = value
 		}
 	}
+	return env
+}
+
+function stringifyEnvVars(env: EnvVars): EnvVars {
+	const result: EnvVars = {}
+	for (const key of Object.keys(env)) {
+		result[`process.env.${key}`] = JSON.stringify(env[key] ?? "")
+	}
+	return result
 }
---
+++ scripts/build-umbrella.ts
@@ -0,0 +1,51 @@
+import * as path from "path"
+import {ConsoleTransport, setLoggerTransports} from "@dvdzkwsk/logger"
+import * as build from "@dvdzkwsk/build"
+
+const WORKDIR = path.join(import.meta.dirname, "..", "packages/umbrella")
+const DEV_MODE = process.argv.includes("--dev")
+const BUILD_MODE = process.argv.includes("--dev") ? "development" : "production"
+
+async function main() {
+	setLoggerTransports([new ConsoleTransport({verbose: false})])
+	await buildClient()
+}
+
+async function buildClient() {
+	const env = await build.loadEnvFile(path.join(WORKDIR, ".env"), {
+		only: [
+			"GOOGLE_OAUTH_CLIENT_ID",
+			"OAUTH_REDIRECT_URI",
+			"SUPABASE_PROJECT_URL",
+			"SUPABASE_PUBLIC_API_KEY",
+		],
+	})
+	const context = build.context({
+		src: path.join(WORKDIR, "client"),
+		dst: path.join(WORKDIR, "dist/client"),
+		entrypoints: [path.join(WORKDIR, "client/main.tsx")],
+		servedir: path.join(WORKDIR, "client/public"),
+		target: "browser",
+		mode: BUILD_MODE,
+		env,
+	})
+	if (DEV_MODE) {
+		await build.serve(context)
+	} else {
+		await build.buildToDisk(context)
+	}
+}
+
+async function buildServer() {
+	const context = build.context({
+		src: path.join(WORKDIR, "server"),
+		dst: path.join(WORKDIR, "dist/server"),
+		entrypoints: [path.join(WORKDIR, "server/main.ts")],
+		target: "node",
+		mode: BUILD_MODE,
+		env: {},
+	})
+	// await build.serve(context)
+}
+
+main()

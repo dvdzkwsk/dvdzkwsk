6c1378086004473722c37da19deed9ce962f3787
David Zukowski <6439050+davezuko@users.noreply.github.com>
2024-02-22 08:49:19 -0800

    fixup blog

--- website/src/About.tsx
+++ website/src/About.tsx
@@ -9,11 +9,10 @@ export const About = () => {
 			<Text>
 				My name is David Zukowski. I'm a software engineer based out of
 				Washington state. I currently work at{" "}
-				<Link href="https://material.security">Material Security</Link>,
-				where we protect sensitive data in your Google and Outlook
-				accounts even if they get compromised. Previously I worked at
-				Microsoft on the new, faster version of Microsft Teams. You can
-				see my full work history{" "}
+				<Link href="https://material.security">Material Security</Link>.
+				We protect your Google and Outlook account, even if it gets
+				hacked. Previously I worked at Microsoft on the new, faster
+				version of Microsoft Teams. You can see my full work history{" "}
 				<Link href="https://www.linkedin.com/in/david-zukowski-0318a475/">
 					here
 				</Link>
@@ -22,7 +21,8 @@ export const About = () => {
 			<Text>
 				My steadfast companion is Kona, the friendliest corgi you'll
 				ever meet. We spend lots of time visiting local parks. Beside
-				that, I enjoy playing guitar, reading, and being outdoors.
+				that, I enjoy playing guitar, hockey, reading, and being
+				outdoors.
 			</Text>
 		</PageLayout>
 	)
